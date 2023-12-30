fd9fcf03dfc9e7d857731b149df2655bd2450d69
BharathS03 <149763876+BharathS03@users.noreply.github.com>
2023-12-30 17:31:41 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const btnshow = document.querySelector('.btn-show');
+const navbar = document.querySelector('.navbar');
+
+btnshow.addEventListener('click',()=>{
+  navbar.classList.toggle('show')
+});
